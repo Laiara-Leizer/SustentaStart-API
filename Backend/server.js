2d55d812b2f0130fb82dedfe91827b15960f9292
Finalshare90 <isaacpmartins08@gmail.com>
2024-10-20 03:37:29 -0300

    Fiz cagada e esqueci de criar branch, mas vai assim mesmo... adicionei a classe inicial de objetos, por favor, seguir ela

--- Backend/server.js
+++ Backend/server.js
@@ -1,12 +1,16 @@
 const express = require('express');
+const Objeto = require("./Objects/objeto");
 
 const app = express();
 const port = 6969;
 
 app.get('/', (req, res) => {
-  res.send('HTestando 123');
+  let myObject = new Objeto(1,"aa",1,"aaa","aaaa",[110,100,100],"#550434");
+  console.log(req.headers);
+  res.send(myObject);
 });
 
 app.listen(port, () => {
-  console.log(`Servidor rodando em http://localhost:${port}`);
+  console.log(`Running on ${port}`);
+  console.log(Objeto);
 });
